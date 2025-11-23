fcf81291a101c3a3e206dbe23de771d88b0aadac
Wendy-Okoth <wendyaokoth@gmail.com>
2025-11-23 14:39:30 +0300

    Customize admin for Member and Instructor with filters, search, and age display

--- members/admin.py
+++ members/admin.py
@@ -2,6 +2,9 @@
 from django.contrib import admin
 from .models import Member
 
-admin.site.register(Member)
-
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'gender', 'date_of_birth', 'age', 'workout_time', 'membership', 'gym_instructor')
+    list_filter = ('gender', 'workout_time', 'membership')
+    search_fields = ('first_name', 'last_name', 'email', 'phone_number')
 
